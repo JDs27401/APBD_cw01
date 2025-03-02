08d72e60df0cf79b78fd4de8fc6bbbcdd396d702
Dominik Juchniewicz <s27401@pjwstk.edu.pl>
2025-03-02 11:24:05 +0100

    static method base

--- zadanie02/zadanie02/Program.cs
+++ zadanie02/zadanie02/Program.cs
@@ -3,3 +3,8 @@ Console.WriteLine("Hello, World!");
 //this is the 1st change to the project
 //this is the 2nd change to the project
 //this is the 3rd and last change to the project
+
+double GetAvg(int[] ints)
+{
+    return 0;
+}
