a3371638d3dc24fae859f4af69d730b928270f13
Dominik Juchniewicz <s27401@pjwstk.edu.pl>
2025-03-02 11:07:11 +0100

    3rd change

--- zadanie02/zadanie02/Program.cs
+++ zadanie02/zadanie02/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //this is the 1st change to the project
+//this is the 2nd change to the project
