4d6e936020621593d15b4c7571453f7a764aa3f0
Dominik Juchniewicz <s27401@pjwstk.edu.pl>
2025-03-16 09:13:27 +0100

    getMax added to branch feature-max

--- zadanie02/zadanie02/Program.cs
+++ zadanie02/zadanie02/Program.cs
@@ -1,20 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 //this is the 1st change to the project
 //this is the 2nd change to the project
 //this is the 3rd and last change to the project
 
-double getAvg(int[] ints)
-{
-    int sum = 0;
+int getMax(int[] ints) {
+    int max = int.MinValue;
     foreach (int i in ints) {
-        sum += i;
+        if (i > max) max = i; 
     }
-    double avg = sum / ints.Length;
-    Console.WriteLine(avg);
-    return avg;
+    return max;
 }
 
-int[] arr = {1, 2, 3,};
-
-double arrAvg = getAvg(arr);
+int[] arr = {1, 2, 4, 3,};
+Console.WriteLine(getMax(arr));
